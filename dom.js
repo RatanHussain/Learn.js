babeb670e3d98e04e7bdaaea9f93efeb49962673
Ratan Hussain <mdratanmia7395@gmail.com>
2023-10-22 00:06:01 +0300

    updated

--- dom.js
+++ dom.js
@@ -1,36 +1,6 @@
-for(let i = 0;i <  3; i++){
-    let btns = document.querySelectorAll('.btn')[i];
-    btns.addEventListener('click', function(){
-        let text = btns.innerHTML;
-        let clas = document.querySelectorAll('.btn')[i];
-        clas.classList.add('btn1');
-        console.log(text)
-        play(text)
-        setTimeout(() => {
-            // location.reload()
-            clas.classList.remove('btn1');
-        }, 2000);
-    })
-}
-
-document.addEventListener('keypress',function(event){
-    let text = event.key;
-    play(text)
-    
+ let count = 0;
+document.querySelector('textarea').addEventListener('keypress',function(event){
+    count++
+    let key = event.key;
+    document.querySelector('p').innerHTML = 'Your message lentg is  ' + key;
 })
-
-
-function play(text){
-    switch(text){
-        case('Play first song'):
-            new Audio('sound/0.mp3').play()
-            break;
-        case('Play the middle song'):
-            new Audio('sound/1.mp3').play()
-            break;
-        case('Play the last song'):
-            new Audio('sound/2.mp3').play()
-            break;
-
-    }
-}
