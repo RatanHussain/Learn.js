53d6f5c21254f648030ccc659f215d6ff7ea9d4c
Ratan Hussain <mdratanmia7395@gmail.com>
2023-10-21 14:04:05 +0300

    update

--- dom.js
+++ dom.js
@@ -1,9 +1,36 @@
-let actor = document.querySelector('#actor')
-
-function remove(){
-    actor.classList.remove('active')
-    actor.classList.remove('active2')
+for(let i = 0;i <  3; i++){
+    let btns = document.querySelectorAll('.btn')[i];
+    btns.addEventListener('click', function(){
+        let text = btns.innerHTML;
+        let clas = document.querySelectorAll('.btn')[i];
+        clas.classList.add('btn1');
+        console.log(text)
+        play(text)
+        setTimeout(() => {
+            // location.reload()
+            clas.classList.remove('btn1');
+        }, 2000);
+    })
 }
-function add(){
-    actor.classList.add('active2')
+
+document.addEventListener('keypress',function(event){
+    let text = event.key;
+    play(text)
+    
+})
+
+
+function play(text){
+    switch(text){
+        case('Play first song'):
+            new Audio('sound/0.mp3').play()
+            break;
+        case('Play the middle song'):
+            new Audio('sound/1.mp3').play()
+            break;
+        case('Play the last song'):
+            new Audio('sound/2.mp3').play()
+            break;
+
+    }
 }
