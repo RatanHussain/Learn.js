fb4ee2578a7611658aff8f36b203553dabeba057
Ratan Hussain <mdratanmia7395@gmail.com>
2023-10-23 15:58:38 +0300

    u

--- dom.js
+++ dom.js
@@ -1,5 +1,18 @@
-function getSelectedOptions() {
-    const selectElement = document.getElementById("multipleSelect");
-    const selectedOptions = Array.from(selectElement.selectedOptions).map(option => option.value);
-    alert("Selected options: " + selectedOptions.join(", "));
-  }
+let form = document.querySelector('form');
+let name = form.querySelector('div #name')
+let email = form.querySelector('div #email')
+let pass = form.querySelector('div #password')
+
+form.addEventListener('submit',function(event){
+  event.preventDefault();
+   console.log(name.value)
+   console.log(email.value)
+   console.log(pass.value)
+
+   const userinfo = {
+    name : name.value,
+    email : email.value,
+    password : pass.value,
+   }
+   console.log(userinfo)
+})
