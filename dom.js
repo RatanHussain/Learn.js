296787fd6dfa56eda0d5716922191054b6a5cb27
Ratan Hussain <mdratanmia7395@gmail.com>
2023-11-28 15:59:48 +0300

    update

--- dom.js
+++
@@ -1,5 +0,0 @@
-function getSelectedOptions() {
-    const selectElement = document.getElementById("multipleSelect");
-    const selectedOptions = Array.from(selectElement.selectedOptions).map(option => option.value);
-    alert("Selected options: " + selectedOptions.join(", "));
-  }
