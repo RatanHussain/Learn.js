f75af62a547477be6bbdcfba871348cb29c0657c
Ratan Hussain <mdratanmia7395@gmail.com>
2023-12-11 23:39:55 +0300

    update

--- form validation/index.js
+++ form validation/index.js
@@ -2,21 +2,29 @@ let form = document.querySelector('#form')
 let fullname = document.querySelector('#name')
 let email = document.querySelector('#email')
 let password = document.querySelector('#password')
-let secondPassord = document.querySelector('#secondPassword')
+let secondPassword = document.querySelector('#secondPassword')
 
 
 
 function showError(input, sms) {
     const parent = input.parentElement;
     const small = parent.querySelector('small')
-
     small.innerText = sms;
-
 }
 
 
-form.addEventListener('submit', (e) => {
-    let len = fullname.slice(/)
 
-    showError(fullname,fullname.lenth)
+
+
+form.addEventListener('submit', (e) => {
+    if (name.length < 5) {
+        showError(fullname,'You must enter at least 5 charactor')
+    }
+
+    if (password.value !== secondPassword.value) {
+        showError(password,`Your password didn't match`)
+        showError(secondPassword,`Your password didn't match`)
+    }
+   
+
 })
