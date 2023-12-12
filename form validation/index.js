43ec2e6271951d306d3c33670536017d2d3a7003
Ratan Hussain <mdratanmia7395@gmail.com>
2023-12-12 15:42:08 +0300

    update

--- form validation/index.js
+++ form validation/index.js
@@ -1,30 +1,45 @@
-let form = document.querySelector('#form')
-let fullname = document.querySelector('#name')
-let email = document.querySelector('#email')
-let password = document.querySelector('#password')
-let secondPassword = document.querySelector('#secondPassword')
+/** @format */
 
-
+let form = document.querySelector('#form');
+let fullname = document.querySelector('#name');
+let email = document.querySelector('#email');
+let password = document.querySelector('#password');
+let secondPassword = document.querySelector('#secondPassword');
 
 function showError(input, sms) {
-    const parent = input.parentElement;
-    const small = parent.querySelector('small')
-    small.innerText = sms;
+	const parent = input.parentElement;
+	const small = parent.querySelector('small');
+	small.innerHTML = sms;
 }
 
-
-
-
-
 form.addEventListener('submit', (e) => {
-    if (name.length < 5) {
-        showError(fullname,'You must enter at least 5 charactor')
-    }
+	e.preventDefault();
 
-    if (password.value !== secondPassword.value) {
-        showError(password,`Your password didn't match`)
-        showError(secondPassword,`Your password didn't match`)
-    }
-   
+	if (fullname.value === '') {
+		showError(fullname, 'Must enter your fullname');
+	} else if (fullname.value.length < 3) {
+		showError(fullname, 'You must enter at least 3 charactor');
+	} else {
+		showError(
+			fullname,
+			`<i class="fa-solid fa-circle-check" style="color:green;"></i>`
+		);
+	}
 
-})
+	if (password.value.length < 6) {
+		showError(password, `Passord must be 6 charactor`);
+		showError(secondPassword, `Passord must be 6 charactor`);
+	} else if (password.value !== secondPassword.value) {
+		showError(password, `Your password didn't match`);
+		showError(secondPassword, `Your password didn't match`);
+	} else {
+		showError(
+			password,
+			`<i class="fa-solid fa-circle-check" style="color:green;"></i>`
+		);
+		showError(
+			secondPassword,
+			`<i class="fa-solid fa-circle-check" style="color:green;"></i>`
+		);
+	}
+});
