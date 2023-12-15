ce50a832fb98408f7d3090057a4ae5461416311d
Ratan Hussain <mdratanmia7395@gmail.com>
2023-12-15 23:19:42 +0300

    update

--- form validation/index.js
+++ form validation/index.js
@@ -1,12 +1,35 @@
 /** @format */
+
+let form = document.querySelector('form');
+let sms = document.querySelector('#sms');
+let sl = document.querySelector('#ul');
 let btn = document.querySelector('#btn');
 
+form.addEventListener('submit', () => {
+	if (sms.value == '') {
+		alert('Enter a value first!');
+	} else {
+		let op = document.createElement('option');
+		op.innerHTML = sms.value;
+		op.id = 'del';
+		op.value = sms.value;
 
-btn.addEventListener('click', () => {
-	let input = document.querySelectorAll("input[name='color']:checked");
-	let ar = [];
-	input.forEach((input) => {
-		ar.push(input.value);
-	});
-	alert(ar);
+		sl.appendChild(op);
+		sms.value = '';
+	}
 });
+
+btn.onclick = () => {
+	let delarry = [];
+
+	for (let i = 0; i < sl.options.length; i++) {
+		delarry[i] = sl.options[i].selected;
+	}
+
+	let index = sl.options.length;
+	while (index--) {
+		if (delarry[index]) {
+			sl.remove(index)
+		}
+	}
+};
