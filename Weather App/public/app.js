faa43c5a818be6e34246889f5d3783621e6bcf9e
Ratan Hussain <mdratanmia7395@gmail.com>
2024-04-03 00:01:27 +0300

    update

--- Weather App/public/app.js
+++ Weather App/public/app.js
@@ -8,25 +8,31 @@ let pressure = document.getElementById('pressure');
 let humidity = document.getElementById('humidity');
 let search = document.getElementById('search');
 let API = 'aac15c6a4ca1b0803073ec55099a595d';
-let url = `https://api.openweathermap.org/data/2.5/weather?appid=${API}`;
+let base_url = `https://api.openweathermap.org/data/2.5/weather?appid=${API}`;
 let icon = `https://openweathermap.org/img/wn/`;
-let defoultCity = 'Riyadh,sa'
-let sendData = {
-    lati: place.coords.latitude,
-    long: place.coords.longitude,
-};
+let defoultCity = 'Riyadh,sa';
 
 document.addEventListener('DOMContentLoaded', function (e) {
-	navigator.geolocation.getCurrentPosition((place) => {
-
-        getWeatherData(null,place)
-    }, e => {
-        getWeatherData(city=defoultCity)
-
-    });
+	navigator.geolocation.getCurrentPosition(
+		(place) => {
+			getWeatherData(null, place.coords);
+		},
+		(e) => {
+			getWeatherData();
+		}
+	);
 });
 
+function getWeatherData(city = defoultCity, coords) {
+	let url = base_url;
+	city === null
+		? (url = `${url}&lat=${coords.latitude}&long=${coords.longitude}`)
+		: (url = `${url}&q=${city}`);
 
-function(city, locat) {
-
+	axios
+		.get(url)
+		.then((respose) => {
+			console.log(respose);
+		})
+		.catch((error) => console.log(error));
 }
